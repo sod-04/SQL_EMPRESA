21905a9f5fd7b342bc516034535757c29dda0ceb
sod-04 <sara.ojeda.ttv.995@unilibre.edu.co>
2024-11-07 17:20:09 -0500

    Creación tabla empleados

--- laboratorio-5_punto-1.sql
+++ laboratorio-5_punto-1.sql
@@ -1,3 +1,11 @@
 ----------- Crear base de datos y registros
 
 CREATE DATABASE empresa;
+
+USE empresa;
+CREATE TABLE empleados( 
+ID_empl INT PRIMARY KEY AUTO_INCREMENT, 
+nombre VARCHAR (50) NOT NULL, 
+cargo VARCHAR (100) NOT NULL, 
+salario VARCHAR (100) NOT NULL 
+);
