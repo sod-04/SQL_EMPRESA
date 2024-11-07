9d0a15bc9c4d18427f8b3feb755e7d4ea3d0ab34
sod-04 <sara.ojeda.ttv.995@unilibre.edu.co>
2024-11-07 17:22:01 -0500

    Inserción de registros en tabla cargo

--- laboratorio-5_punto-1.sql
+++ laboratorio-5_punto-1.sql
@@ -23,3 +23,9 @@ INSERT INTO empleados (nombre, cargo, salario)
 VALUES ('Juan Pérez', 'Gerente', '7500000'), 
 ('Ana Gómez', 'Desarrollador', '6000000'), 
 ('Luis Castro','Proyectos', '10000000');
+
+USE empresa;
+INSERT INTO cargo (nombre_cargo,salario)
+VALUES ('Gerente', '500000'),
+('Asistente', '250000'),
+('Desarrollador', '400000');
