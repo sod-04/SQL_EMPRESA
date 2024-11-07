3402086a5fd0397b7b9e0cb70fc6bf87c2292bb6
sod-04 <sara.ojeda.ttv.995@unilibre.edu.co>
2024-11-07 17:20:56 -0500

    Creación tabla cargo

--- laboratorio-5_punto-1.sql
+++ laboratorio-5_punto-1.sql
@@ -9,3 +9,11 @@ nombre VARCHAR (50) NOT NULL,
 cargo VARCHAR (100) NOT NULL, 
 salario VARCHAR (100) NOT NULL 
 );
+
+USE  empresa;
+CREATE TABLE cargo (
+    ID_cargo INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre_cargo VARCHAR (100) NOT NULL,
+    Descripcion VARCHAR (200),
+    Salario VARCHAR (100) NOT NULL
+);
